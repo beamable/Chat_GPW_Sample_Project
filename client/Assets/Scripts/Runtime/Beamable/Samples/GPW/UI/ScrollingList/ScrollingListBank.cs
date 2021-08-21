5fc34aeb6945559719181f8e5d5ccbfe84be6e3f
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2021-08-21 11:11:17 -0600

    Add ScrollingListDemo

---
+++ client/Assets/Scripts/Runtime/Beamable/Samples/GPW/UI/ScrollingList/ScrollingListBank.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using AirFishLab.ScrollingList;
+using UnityEngine;
+
+namespace Beamable.Samples.GPW.UI
+{
+    [Serializable]
+    public class ColorString
+    {
+        public Color color;
+        public string name;
+    }
+    
+    /// <summary>
+    /// Data provider of ScrollingList's <see cref="BaseListBank"/>.
+    /// </summary>
+    public class ScrollingListBank : BaseListBank
+    {
+        //  Properties  ----------------------------------
+        public long LocalPlayerDbid { get { return _localPlayerDbid; } set { _localPlayerDbid = value; } }
+
+        //  Fields  --------------------------------------
+        private long _localPlayerDbid;
+        
+        [SerializeField]
+        private ColorString[] _contents;
+
+        //  Other Methods  --------------------------------
+        public override object GetListContent(int index)
+        {
+            return _contents[index];
+        }
+
+        public override int GetListLength()
+        {
+            return _contents.Length;
+        }
+        
+        //  Event Handlers  -------------------------------
+    }
+}
---
+++ client/Assets/Scripts/Runtime/Beamable/Samples/GPW/UI/ScrollingList/ScrollingListBox.cs
@@ -0,0 +1,40 @@
+﻿using AirFishLab.ScrollingList;
+using Beamable.Samples.GPW.Data;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Beamable.Samples.GPW.UI
+{
+    public class RefreshEvent : UnityEvent<GameService>{}
+	
+    /// <summary>
+    /// Renderer of ScrollingList's <see cref="ListBox"/>.
+    /// </summary>
+    public class ScrollingListBox : ListBox
+    {
+        //  Events  --------------------------------------
+        public RefreshEvent OnRefresh = new RefreshEvent();
+		
+        //  Properties  ----------------------------------
+        public Text Text { get { return _contentText; }  }
+
+        //  Fields  --------------------------------------
+        [SerializeField]
+        private Image _contentImage;
+        
+        [SerializeField]
+        private Text _contentText;
+        
+        //  Other Methods  --------------------------------
+        protected override void UpdateDisplayContent(object content)
+        {
+            var colorString = (ColorString) content;
+            _contentImage.color = colorString.color;
+            _contentText.text = colorString.name;
+        }
+        
+        //  Event Handlers  -------------------------------
+    }
+}
+
