95c8639ad9dadd207e9e4d5c34da86175e74ad6a
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2021-08-21 10:45:33 -0600

    Add 3rdParty: CircularScrollingList

---
+++ client/Assets/3rdParty/AirFishLab/CircularScrollingList/_Demo/Scripts/FewerContentListBank.cs
@@ -0,0 +1,19 @@
+﻿namespace AirFishLab.ScrollingList.Demo
+{
+    public class FewerContentListBank : BaseListBank
+    {
+        private int[] contents = {
+            1, 2, 3,
+        };
+
+        public override object GetListContent(int index)
+        {
+            return contents[index];
+        }
+
+        public override int GetListLength()
+        {
+            return contents.Length;
+        }
+    }
+}
---
+++ client/Assets/3rdParty/AirFishLab/CircularScrollingList/readme.txt
@@ -0,0 +1,5 @@
+Source Url: 
+* https://assetstore.unity.com/packages/tools/gui/circular-scrolling-list-187418
+
+Changelog: 
+* Moved to /Assets/3rdParty/
